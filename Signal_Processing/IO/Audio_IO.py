3004e39349d87128bcd1f81c1acce0b26a1a5368
Carlos <c.sotogd@gmail.com>
2022-11-20 11:55:30 +0100

    Reading freom mic in parallel from the mic WORKING!

--- Signal_Processing/IO/Audio_IO.py
+++ Signal_Processing/IO/Audio_IO.py
@@ -13,17 +13,30 @@ class Parameters_IO:
 
     channels = 1 # is normally 2, one for each speaker, we set it to one as it would be quite complex otherwise.
     fs = None #to be initialized
-    recorded_so_far =[]
-
-def  read_segment(seconds):
-    fs = Parameters_IO.fs
-
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=Parameters_IO.channels)
-    sd.wait()  # Wait until recording is finished
-    write('output.wav', fs, myrecording)  # Save as WAV file
-    return myrecording
 
 
+class Segment_reader:
+    def __init__(self, total_seconds_to_record):
+        self.recorded_so_far = []
+        self.__seconds_recorded_so_far = 0
+        self.total_seconds_to_record = total_seconds_to_record
+        self.last_read_segment = np.array([])
+
+    def  read_segment(self, seconds):
+        fs = Parameters_IO.fs
+
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=Parameters_IO.channels)
+        sd.wait()  # Wait until recording is finished
+        self.recorded_so_far.append(myrecording)
+        #write('output.wav', fs, myrecording)  # Save as WAV file
+        self.last_read_segment= myrecording.T
+
+
+    def get_recorded_so_far(self):
+        """"
+        Returns a numpy array with the recorded signal so far up to now.
+        """
+        return np.concatenate(self.recorded_so_far, axis=0)
 
 
 def do_something():
@@ -32,6 +45,7 @@ def do_something():
     print('Done sleeping')
 
 if __name__ == '__main__':
+    """"
     Parameters_IO.fs = 45000
     read_segment(2)
 
@@ -50,3 +64,4 @@ if __name__ == '__main__':
 
     finish = time.perf_counter()
     print(f'Finished in {round(finish - start, 2)} seconds')
+    """
--- main/main.py
+++ main/main.py
@@ -3,6 +3,8 @@ from Uttilities.Generator_segments import Generator_segments_recorded
 from scipy.io.wavfile import read
 from Signal_Processing.signal_plotting import plot_signal
 from Uttilities.pipelines import *
+from Signal_Processing.IO.Audio_IO import Segment_reader
+import threading
 
 if __name__ == '__main__':
 
@@ -18,12 +20,48 @@ if __name__ == '__main__':
     #now we focus on the signal that is recorded by the microphone. There are two posibilities:
     #we either have the song stored because it is a past recording and we need to load it or we
     #will record the sound in real time. This will be controlled by the variable from_mic
-    from_mic = False
+    from_mic = True
     path_to_recording = "../data/imagine_john_lennon_PIANO.wav"
     #since we have not recorded Diyon playing piano, we will match the signal with itself
 
     if from_mic:
-        a= "NEEDS TO BE IMPLEMENTED"
+        seconds_per_segment = 2
+        seconds_so_far = 0
+        total_seconds =10
+        mic_segment_reader = Segment_reader(total_seconds_to_record=total_seconds)
+
+
+        #First iteration can not be done in parallel as there is nothing to proccess
+        #because nothing has been recorded yet
+        mic_segment_reader.read_segment(seconds=seconds_per_segment)
+        segment_mic_previous = mic_segment_reader.last_read_segment
+        seconds_so_far += seconds_per_segment
+
+        while (seconds_so_far < total_seconds):
+            #now both the recording from the mic and the preproccessing operation for the previously read
+            #segment have to be performed in parallel. For that we create multithreading (shared memory)
+            t1 = threading.Thread(target=sp_pipeline,
+                                  args= [segment_mic_previous, Parameters_IO, True])
+            #TODO include matching algo here
+
+            t2 = threading.Thread(target=mic_segment_reader.read_segment,
+                                  args=[seconds_per_segment])
+
+            t1.start()
+            t2.start()
+
+            t1.join()
+            t2.join()
+
+            seconds_so_far+= seconds_per_segment
+            segment_mic_previous = mic_segment_reader.last_read_segment
+
+        #once it is done with the loop it still needs to proccess the last recorded segment
+        sp_pipeline(segment_mic_previous, Parameters_IO.fs, denoise=True)
+        #TODO include matching part here
+
+
+
 
     else:
         # Read the input WAV files
@@ -40,7 +78,7 @@ if __name__ == '__main__':
         song_segment = generator_semnets.next()
         while song_segment is not None:
             plot_signal(song_segment)
-            sp_pipeline(song_segment, Fs, denoise=True)
+            sp_pipeline(song_segment, Parameters_IO.fs, denoise=True)
             #TODO include matching algorithm here
             #here we would compare histograms
 
