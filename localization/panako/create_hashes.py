0b8d6af25f3da990faa6fd87e3657a77522dca64
pk-470 <parmenkouts99@gmail.com>
2023-01-11 13:37:41 +0100

    changed t_2 and freq_2 to come from future point

--- localization/panako/create_hashes.py
+++ localization/panako/create_hashes.py
@@ -17,8 +17,8 @@ Constants required for Panako:
 - Time dilation: The percentage by which the time should be dilated
 """
 FREQ_SECTOR_SIZE = UPPER_FREQ / 8
-FREQ_SHIFT = 0.9
-TIME_DILATION = 0.95
+# FREQ_SHIFT = 0.9
+# TIME_DILATION = 0.95
 
 
 def create_hashes(constellation_map: list):
@@ -38,43 +38,47 @@ def create_hashes(constellation_map: list):
         the corresponding hash is encountered.
     """
 
-
     index_dict = {}
     hashes_list = []
 
     arr_ind = 0
 
     for i, (t_0, freq_0) in enumerate(constellation_map):
-        for t_1, freq_1 in constellation_map[i: i + 10]:
+        for j, (t_1, freq_1) in enumerate(constellation_map[i : i + 10]):
+            for t_2, freq_2 in constellation_map[j : j + 10]:
 
-            # Shift frequency and time
-            freq_2 = freq_0 * FREQ_SHIFT
-            t_2 = t_0 * TIME_DILATION
+                # 4 bits
+                freq_diff_1 = ((freq_0 - freq_1) / UPPER_FREQ) * (2**FREQ_BITS)
+                # 4 bits
+                freq_diff_2 = ((freq_1 - freq_2) / UPPER_FREQ) * (2**FREQ_BITS)
+                # 4 bits
+                f0_tilde = int(freq_0 / FREQ_SECTOR_SIZE)
+                # 4 bits
+                f2_tilde = int(freq_2 / FREQ_SECTOR_SIZE)
 
-            # 4 bits
-            freq_diff_1 = ((freq_0 - freq_1) / UPPER_FREQ) * (2 ** FREQ_BITS)
-            # 4 bits
-            freq_diff_2 = ((freq_1 - freq_2) / UPPER_FREQ) * (2 ** FREQ_BITS)
-            # 4 bits
-            f0_tilde = int(freq_0 / FREQ_SECTOR_SIZE)
-            # 4 bits
-            f2_tilde = int(freq_2 / FREQ_SECTOR_SIZE)
+                # Shifting the starting time/origin has no effect, therefore will always present a division by 0 error
+                # 4 bits
+                t_ratio = 0 if t_2 - t_0 == 0 else (abs(t_1 - t_0) / abs(t_2 - t_0))
+                # 4 bits
+                td = t_2 - t_0
 
-            # Shifting the starting time/origin has no effect, therefore will always present a division by 0 error
-            # 4 bits
-            t_ratio = 0 if t_0 == 0 and t_2 == 0 else (abs(t_1 - t_0) / abs(t_2 - t_0))
-            # 4 bits
-            td = (t_2 - t_0)
+                # 4 bits
+                freq_0_binned = freq_0 / UPPER_FREQ * (2**FREQ_BITS)
 
-            # 4 bits
-            freq_0_binned = freq_0 / UPPER_FREQ * (2 ** FREQ_BITS)
+                # 32-bit hash
+                hash = (
+                    int(freq_diff_1)
+                    | int(freq_diff_2) << 4
+                    | f0_tilde << 8
+                    | f2_tilde << 12
+                    | int(t_ratio) << 16
+                    | int(t_0) << 20
+                    | int(freq_0_binned) << 24
+                    | int(td) << 28
+                )
 
-            # 32-bit hash
-            hash = int(freq_diff_1) | int(freq_diff_2) << 4 | f0_tilde << 8 | f2_tilde << 12 | \
-                int(t_ratio) << 16 | int(t_0) << 20 | int(freq_0_binned) << 24 | int(td) << 28
-
-            hashes_list.append(hash)
-            index_dict[arr_ind] = t_0
-            arr_ind += 1
+                hashes_list.append(hash)
+                index_dict[arr_ind] = t_0
+                arr_ind += 1
 
     return np.asarray(hashes_list).astype(np.uint64), index_dict
