de31bd38853055f4670465edd00722b509b230eb
PC\csoto <c.sotogd@gmail.com>
2023-01-12 15:44:57 +0100

    generating conditional probability correctly

--- localization/montecarlo_robot/montecarlo.py
+++ localization/montecarlo_robot/montecarlo.py
@@ -1,5 +1,6 @@
 import random
-
+from numpy import arange
+import numpy as np
 def create_initial_set_of_particles(nr_particles, length_ref_subset):
     """
     Creates and return an initial set of particles S_0. This is done by uniformly randomly selecting 
@@ -29,7 +30,7 @@ def update_phase():
     generate_weights_m_i_k()
     generate_new_set_of_particles_update_phase()
 
-def generate_prob_xk_of_prediction_phase(particle, step_size=0.1):
+def generate_prob_xk_of_prediction_phase(particle,length_side ,length_ref,step_size=0.1):
     """
     Here we are calculating the probability of being in a certain point knowing the resulting set of particles in the previous phase and
     not knowing the input at time k. This is the probability distribution that is calculated under the prediction phase of the paper. 
@@ -55,34 +56,65 @@ def generate_prob_xk_of_prediction_phase(particle, step_size=0.1):
     Parameters
     ----------
     particle: a time point in seconds (float)
+    length_side: float length in seconds of each side of the interval
+    length_ref: length of reference song in seconds.
     step_size: float step size in seconds. We will generate the probability of selecting timepoints every step_size seconds
 
     Returns
     -------
-    A dict where the keys are time points and the values are the probabilities of salecting those keys.
+    A np array of dimensions [nr_steps in interval, where for each entry i,
+            the first element [i][0] is a time point and the second element is the probability of selecting that time point.
+            If the probability for a value is 0 there will be no entry for it
     """
+    interval_nr_indices = int((length_side*2)/step_size)+1
+    probs_x= np.zeros([interval_nr_indices , 2], dtype=float)
 
-    return "to be implemented"
+    start_second = max(0, particle- length_side)
+    end_second = min(particle+ length_side, length_ref)
 
-def generate_new_particle_prediction_phase(probs_x):"""
+    slope = 1/length_side
+    i=0
+    for time in arange(start_second, particle, step_size): #first lenght of the interval
+        probs_x[i][0]=time
+        probs_x[i][1]=(time-start_second)*slope
+        i+=1
+
+    for time in arange(particle,end_second, step_size): #first lenght of the interval
+        probs_x[i][0]= time
+        probs_x[i][1]    = 1 + (time- particle)* - slope
+        i += 1
+
+    print(sum(probs_x[:,1]))
+    #now we normalize the probability
+    probs_x[:,1] /=sum(probs_x[:,1])
+    print(sum(probs_x[:,1]))
+    return probs_x
+
+def generate_new_particle_prediction_phase(probs_x):
+    """
     Generates a new particle knowing the probabilities of x given a particle and the previous input. This is part of the prediction phase
     
     Parameters
     ----------
     probs_x: conditional probability of having a particle at state x calculated previously in this prediciton phase
+            A np array of dimensions [step_size* 2*length_Side][2], where for each entry i, 
+            the first element [i][0] is a time point and the second element is the probability of selecting that time point. 
+            If the probability for a value is 0 there will be no entry for it
 
     Returns
     -------
     a new particle (float), which represents a time point.
     """
+    return ""
+
+
 
 
 def generate_weights_m_i_k():
+    return "implement"
 
 def generate_new_set_of_particles_update_phase():
-
+    return "implement"
 
 if __name__ == '__main__':
-    particles = create_initial_set_of_particles(nr_particles=100, length_ref_subset=10)
-    print(len(particles))
-    print(particles)
+    (generate_prob_xk_of_prediction_phase(particle=5, length_side=3, length_ref=10, step_size=0.1))
