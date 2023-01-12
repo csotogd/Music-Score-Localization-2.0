756aaea1a9725976b031f6d7790168e24c0bd69d
PC\csoto <c.sotogd@gmail.com>
2023-01-12 14:38:28 +0100

    created new mc loc branch

---
+++ localization/montecarlo_robot/montecarlo.py
@@ -0,0 +1,88 @@
+import random
+
+def create_initial_set_of_particles(nr_particles, length_ref_subset):
+    """
+    Creates and return an initial set of particles S_0. This is done by uniformly randomly selecting 
+    points in time between 0 and the length of the reference song.
+    Parameters
+    ----------
+    nr_particles int
+    length_ref_subset int. This is in seconds. Either the length of the refernece song (if we are to compute it fully) or the size of the
+                            subset we are going to consider
+
+    Returns
+    -------
+    A list of time points in seconds.
+    """
+
+    particles = [((random.random()+0) *length_ref_subset) for _ in range(nr_particles)]
+
+    return particles
+
+def prediction_phase(S_k_minus_1):
+    for i in range(len(S_k_minus_1)):
+        particle = S_k_minus_1[i]
+        probs_x= generate_prob_xk_of_prediction_phase(particle)
+        generate_new_particle_prediction_phase()
+
+def update_phase():
+    generate_weights_m_i_k()
+    generate_new_set_of_particles_update_phase()
+
+def generate_prob_xk_of_prediction_phase(particle, step_size=0.1):
+    """
+    Here we are calculating the probability of being in a certain point knowing the resulting set of particles in the previous phase and
+    not knowing the input at time k. This is the probability distribution that is calculated under the prediction phase of the paper. 
+    
+    In order to calculate the probability, we do the following:
+    Each particle has an associated interval for which there is a score. The particle is centered at the beggining of the interval and the score
+    throughout the interval decreases linearly. Here is an example:
+
+          |                    *
+    score |                   / \
+          |                  /   \
+          |                 /     \
+          |________________/_______\______________
+               time        a        b
+
+    Anything between a and b would get a score
+
+
+    So the proccess works as follows:
+    1.- We take steps of step_size seconds. For each step we calculate a score for that position as explained above.
+    2.- We sum the scores of each position and we normalize. This way we get a probability distribution between 0 and 1
+
+    Parameters
+    ----------
+    particle: a time point in seconds (float)
+    step_size: float step size in seconds. We will generate the probability of selecting timepoints every step_size seconds
+
+    Returns
+    -------
+    A dict where the keys are time points and the values are the probabilities of salecting those keys.
+    """
+
+    return "to be implemented"
+
+def generate_new_particle_prediction_phase(probs_x):"""
+    Generates a new particle knowing the probabilities of x given a particle and the previous input. This is part of the prediction phase
+    
+    Parameters
+    ----------
+    probs_x: conditional probability of having a particle at state x calculated previously in this prediciton phase
+
+    Returns
+    -------
+    a new particle (float), which represents a time point.
+    """
+
+
+def generate_weights_m_i_k():
+
+def generate_new_set_of_particles_update_phase():
+
+
+if __name__ == '__main__':
+    particles = create_initial_set_of_particles(nr_particles=100, length_ref_subset=10)
+    print(len(particles))
+    print(particles)
